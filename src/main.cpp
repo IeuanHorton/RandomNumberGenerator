bf4b3e28bb22d4b6e994cac541f283f6a1b3d1a8
IeuanHorton <HortonIeuan@gmail.com>
2025-07-29 23:58:09 +0000

    basicly done tbh

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include <iostream> // For input/output operations (e.g., cout)
+#include <cstdlib>  // For rand() and srand()
+#include <ctime>    // Allows time() to exist
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello world!" << std::endl;
+    int givenNumber = -1;
+    int responseNumber = -1;
+    std::cout << "Hello! Plesae choose a number to randomize to:" ;
+    std::cin >> givenNumber;
+    srand(static_cast<unsigned int>(time(0)));
+    responseNumber = (rand() & givenNumber) + 1;
+    std::cout << "The random number I have chosen: " << responseNumber << std::endl;
+    return 0;
 }
